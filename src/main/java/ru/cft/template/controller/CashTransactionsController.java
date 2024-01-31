0b90d3a143ff73cfcab9380a4da84cda9e3e83eb
Alexander Kirsanov <alialex58@mail.ru>
2024-01-31 17:25:10 +0700

    Добавлены шаблоны для сервисов транзакций

---
+++ src/main/java/ru/cft/template/controller/CashTransactionsController.java
@@ -0,0 +1,12 @@
+package ru.cft.template.controller;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+@Data
+public class CashTransactionsController {
+
+}
